8b6d94fa5624918a8e86ac3dd3a4b6aa59ae9c81
muditsharma01 <muditshar001@gmail.com>
2023-03-26 15:19:43 +0530

    Update Pads.js

--- src/components/Pads.js
+++ src/components/Pads.js
@@ -1,75 +1,32 @@
-import React, { useState } from 'react';
-import Pad from './Pad';
-import { bank1 } from './App';
+import React from "react";
+import Pad from "./Pad";
+import { bank1 } from "./App";
 
-function Pads() {
+function Pads({ power, volume }) {
   const keypadCode = Object.keys(bank1);
 
-  const [power, setPower] = useState(true);
-  const [volume, setVolume] = useState(0.5);
-  const [currentClip, setCurrentClip] = useState('');
-
-  const handlePowerToggle = () => {
-    setPower(!power);
-    setCurrentClip('');
-  };
-
-  const handleVolumeChange = e => {
-    const newVolume = e.target.value;
-    setVolume(newVolume);
-  };
-
-  const playSound = (clipName, clipId) => {
-    if (power) {
-      const audioElement = document.getElementById(clipName);
-      audioElement.currentTime = 0;
-      audioElement.volume = volume;
-      audioElement.play();
-      setCurrentClip(clipId);
-    }
+  const playSound = (e) => {
+    const audio = document.getElementById(e.target.innerText);
+    audio.volume = volume;
+    audio.currentTime = 0;
+    audio.play();
+    document.getElementById("display").innerText = bank1[e.target.innerText].name;
   };
 
   return (
-    <div id="drum-machine">
-      <div id="div-pads">
-        {keypadCode.map((pad, idx) => (
+    <div id="div-pads">
+      {keypadCode.map((pad, idx) => {
+        return (
           <Pad
-            id={`pad-${pad}`}
-            key={`pad-${pad}`}
-            handleClick={() => playSound(pad, bank1[pad].id)}
+            id={pad + idx}
+            key={pad + idx}
+            handleClick={playSound}
             element={pad}
             power={power}
-            volume={volume}
-            className={currentClip === bank1[pad].id ? 'active' : ''}
           />
-        ))}
-      </div>
-      <div id="controls">
-        <div className="control">
-          <p>Power</p>
-          <div className="toggle-control">
-            <input
-              type="checkbox"
-              id="power-toggle"
-              checked={power}
-              onChange={handlePowerToggle}
-            />
-            <label htmlFor="power-toggle" className="toggle"></label>
-          </div>
-        </div>
-        <div className="control">
-          <p>Volume</p>
-          <input
-            type="range"
-            min="0"
-            max="1"
-            step="0.01"
-            value={volume}
-            onChange={handleVolumeChange}
-          />
-        </div>
-      </div>
-      <div id="display">{currentClip}</div>
+        );
+      })}
+      <div id="display">Play a sound!</div>
     </div>
   );
 }
